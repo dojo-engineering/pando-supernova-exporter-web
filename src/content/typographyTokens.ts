9cdb20ff50a04a730d1c860c15f0635e8d6fc0bf
maemastersdojo <116581700+maemastersdojo@users.noreply.github.com>
2024-07-23 16:13:37 +0100

    update typography token property names, rm unecessary imports,

--- src/content/typographyTokens.ts
+++ src/content/typographyTokens.ts
@@ -1,18 +1,15 @@
 import { TypographyToken } from "@supernovaio/sdk-exporters";
-import { CSSHelper, ColorFormat } from "@supernovaio/export-helpers";
-import { REFERENCABLE_TOKEN_TYPES } from "@supernovaio/sdk-exporters/build/sdk-typescript/src/model/enums/SDKTokenType";
 
 export function buildTypographyToken(token: TypographyToken, mappedTokens): {} | null {
     const fontFamily = token.value.fontFamily.text;
     const fontWeight = token.value.fontWeight.text;
-    const fontSize = `${token.value.fontSize.measure}rem`;
+    const fontSize = `${token.value.fontSize.referencedTokenId}`;
     const lineHeight = `${token.value.lineHeight?.measure}%`;
     const textTransform = token.value.textCase.value.toLowerCase();
     const textDecoration = token.value.textDecoration.value.toLowerCase();
     const letterSpacing = `${token.value.letterSpacing.measure}%`;
     const paragraphSpacing = `${token.value.paragraphSpacing.measure}px`;
 
-    const remToPx = (rem: number) => rem * 16;
     return {
         fontFamily,
         fontWeight,
